5cbec101c8eb6f2ae86d769e3672a541d8fdfada
Eric Hu <eric@lemurheavy.com>
2013-08-26 15:25:28 -0700

    Refactor RdebugOptionParser into a singleton class, add tests

--- test/rdebug/rdebug_option_parser_test.rb
+++ test/rdebug/rdebug_option_parser_test.rb
@@ -1,14 +1,48 @@
 require_relative '../test_helper'
-require 'debugger/runner/rdebug_option_parser'
+require_relative '../../lib/debugger/runner/rdebug_option_parser'
 require 'minitest/autorun'
 
 describe RdebugOptionParser do
-  it "receives options to parse" do
-    mock_obj = Minitest::Mock.new
-    mock_obj.expect(:process_options, nil, [anything])
-    #mock_obj.verify
-    assert_send([RdebugOptionParser, :process_options, *parameters])
-    #RdebugOptionParser.should_receive(:process_options).with(anything)
-    RdebugOptionParser.begin
+  it "returns an option summary" do
+    RdebugOptionParser.instance.to_s.wont_be_empty
+  end
+
+  describe 'verbose options' do
+    before do
+      # preserve $VERBOSE setting so that this test doesn't make other tests
+      # noisy
+      @verbose_before = $VERBOSE
+    end
+
+    after do
+      $VERBOSE = @verbose_before
+    end
+
+    it 'supports a verbose option' do
+      options = RdebugOptionParser.instance.parse ['--verbose']
+
+      options.verbose_long.must_equal true
+      $VERBOSE.must_equal true
+    end
+
+    it 'supports a verbose/version option' do
+      out, err = capture_io do
+        options = RdebugOptionParser.instance.parse ['-v']
+
+        $VERBOSE.must_equal true
+      end
+
+      out.must_include Debugger::VERSION
+    end
+  end
+
+  it 'supports a version option' do
+    out, err = capture_io do
+      assert_raises(SystemExit) {
+          RdebugOptionParser.instance.parse ['--version']
+      }
+    end
+
+    out.must_include Debugger::VERSION
   end
 end
